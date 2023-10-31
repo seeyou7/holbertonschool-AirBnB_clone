019ae585bbb0a6b5275fe30998060d317ae7b974
seeyou7 <>
2023-10-31 08:13:16 -0700

    class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" first class base """
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ base cls """
+
+    def __init__(self):
+        """ Constructor for BaseModel """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """ to save the updated d.time """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+            Return a dictionary representation of the instance.
+            returns:
+                dict: A dictionary containing instance attributes and a __class__ key.
+
+            Converts created_at and updated_at to string objects in ISO format.
+        """
+        class_name = self.__class__.__name__
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = class_name
+
+        """ to convert """
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.created_at.isoformat()
+        return instance_dict
+
+    def __str__(self):
+        """ Return a string representation of the instance """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
