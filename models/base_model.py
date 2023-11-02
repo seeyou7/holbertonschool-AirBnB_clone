366730bc72f78a920906e3602bc15bc077f4c82c
Aicha-ch <chwikhi.aycha@gmail.com>
2023-11-02 04:08:28 -0700

    update the constructor

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,24 @@ from datetime import datetime
 class BaseModel:
     """ base cls """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Constructor for BaseModel """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+             for key, value in kwargs.items():
+                 if key == "__class__":
+                     continue
+
+                 if key in ["created_at", "updated_at"]:
+                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                 setattr(self, key, value)
+        else:
+            """if kwargs is empty"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def save(self):
         """ to save the updated d.time """
