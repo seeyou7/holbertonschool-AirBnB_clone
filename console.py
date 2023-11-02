e294c773b24617c1943c51ac56731f530422c2c8
seeyou7 <>
2023-11-02 04:54:54 -0700

    fix exit message

--- console.py
+++ console.py
@@ -10,11 +10,11 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb)'
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """ Quit command to exit the program """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """ exit the l.cmd using cntrl D """
         print()
         return True
