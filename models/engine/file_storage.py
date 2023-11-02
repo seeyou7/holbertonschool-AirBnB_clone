b43f571aa315df48a6ea3e93c810e3ce52b4224e
Aicha-ch <chwikhi.aycha@gmail.com>
2023-11-02 11:47:05 -0700

    update FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,7 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             FileStorage.__objects[key] = obj
 
     def save(self):
@@ -32,21 +32,17 @@ class FileStorage:
         serialized_objects = {}
 
         for key, obj in FileStorage.__objects.items():
-
             serialized_objects[key] = obj.to_dict()
+
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
+        """deserializes the JSON file to __objects"""
         try:
-            if path.exists(FileStorage.__file_path):
-                with open(FileStorage.__file_path, 'r') as file:
-                    json_data = json.load(file)
-                    for key, obj_data in json_data.items():
-                        class_name, obj_id = key.split('.')
-                        cls = globals()[class_name]
-                        obj = cls(**obj_data)
-                        FileStorage.__objects[key] = obj
-        except Exception as e:
-            print("Error during reload: {}".format(str(e)))
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
+            pass
