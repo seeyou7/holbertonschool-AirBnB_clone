b8221b5750f5ec6fba7cf6dddb4bf217fc0e54a6
seeyou7 <>
2023-11-03 11:02:29 -0700

    fix pycode style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Filestorage class"""
 
-import models
 import json
-from os import path
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
